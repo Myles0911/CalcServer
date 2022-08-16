9abd6fe4821cea559d8ae8b79b2137a7c5577960
Myles D. Walker <mdwalker1@aggies.ncat.edu>
2022-08-16 11:35:00 -0400

    Initial commit

---
+++ src/test/java/dev/Walker/tests/ExampleTests.java
@@ -0,0 +1,19 @@
+package dev.Walker.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTests {
+    @Test
+    void test1() {
+        System.out.println("PASS");
+    }
+    @Test
+    void test2() {
+        System.out.println("PASS");
+    }
+
+    @Test
+    void test3() {
+        System.out.println("FAIL");
+    }
+}
